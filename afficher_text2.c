299f671a344ded54d902892d63fcfcf95593c11c
phinpharin <phinpharin@gmail.com>
2016-08-20 18:06:25 +0200

    test

---
+++ afficher_text2.c
@@ -0,0 +1,30 @@
+</*
+ * afficher_text2.c
+ * 
+ * Copyright 2016 phin <phin@phin-ThinkCentre-M58e>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf ("Dennis Ritchie a dit un jour :\n\"La meilleure façon d'apprendre à programmer, c'est de programmer.\"");
+    return 0;
+}
+
